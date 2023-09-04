0df8fa272cc8ed9c3ad3a17305ced604ff096e91
eithandarmon <eithandarmon9911@gmail.com>
2023-09-04 23:58:58 +0630

    index update

--- resources/views/user_layouts/best_seller.blade.php
+++ resources/views/user_layouts/best_seller.blade.php
@@ -1,7 +1,7 @@
- <div class="u-s-p-b-60">
+ <!-- <div class="u-s-p-b-60"> -->
 
   <!--====== Section Intro ======-->
-  <div class="section__intro u-s-m-b-46">
+  <!-- <div class="section__intro u-s-m-b-46">
    <div class="container">
     <div class="row">
      <div class="col-lg-12">
@@ -13,26 +13,26 @@
      </div>
     </div>
    </div>
-  </div>
+  </div> -->
   <!--====== End - Section Intro ======-->
 
 
   <!--====== Section Content ======-->
-  <div class="section__content">
+  <!-- <div class="section__content">
    <div class="container">
     <div class="row">
      <div class="col-lg-4 col-md-6 u-s-m-b-30">
       <div class="bp-mini bp-mini--img u-h-100">
-       <div class="bp-mini__thumbnail">
+       <div class="bp-mini__thumbnail"> -->
 
         <!--====== Image Code ======-->
 
-        <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html">
+        <!-- <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html">
 
-         <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_2.jpg')}}" alt=""></a>
+         <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_2.jpg')}}" alt=""></a> -->
         <!--====== End - Image Code ======-->
-       </div>
-       <div class="bp-mini__content">
+       <!-- </div>
+       <div class="bp-mini__content"> -->
         <!-- <div class="bp-mini__stat">
 
                                             <span class="bp-mini__stat-wrap">
@@ -70,7 +70,7 @@
 
                                             <a>Health</a>
                                         </div> -->
-
+<!-- 
         <span class="bp-mini__h1">
 
          <a href="blog-detail.html">Life is an extraordinary Adventure</a></span>
@@ -79,25 +79,25 @@
 
          <a class="gl-tag btn--e-transparent-hover-brand-b-2">View</a>
 
-         <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a>
+         <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a> -->
 
          <!-- <a class="gl-tag btn--e-transparent-hover-brand-b-2">Place</a> -->
-        </div>
+        <!-- </div>
        </div>
       </div>
      </div>
      <div class="col-lg-4 col-md-6 u-s-m-b-30">
-      <div class="bp-mini bp-mini--img u-h-100">
-       <div class="bp-mini__thumbnail">
+      <div class="bp-mini bp-mini--img u-h-100"> -->
+       <!-- <div class="bp-mini__thumbnail"> -->
 
         <!--====== Image Code ======-->
 
-        <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html">
+        <!-- <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html"> -->
 
-         <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_12.jpg')}}" alt=""></a>
+         <!-- <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_12.jpg')}}" alt=""></a> -->
         <!--====== End - Image Code ======-->
-       </div>
-       <div class="bp-mini__content">
+       <!-- </div> -->
+       <!-- <div class="bp-mini__content"> -->
         <!-- <div class="bp-mini__stat">
 
                                             <span class="bp-mini__stat-wrap">
@@ -135,7 +135,7 @@
 
                                             <a>Health</a>
                                         </div> -->
-
+<!-- 
         <span class="bp-mini__h1">
 
          <a href="blog-detail.html">Wait till its open</a></span>
@@ -144,24 +144,24 @@
 
          <a class="gl-tag btn--e-transparent-hover-brand-b-2">View</a>
 
-         <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a>
+         <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a> -->
 
          <!-- <a class="gl-tag btn--e-transparent-hover-brand-b-2">Place</a> -->
-        </div>
+        <!-- </div>
        </div>
       </div>
      </div>
      <div class="col-lg-4 col-md-6 u-s-m-b-30">
       <div class="bp-mini bp-mini--img u-h-100">
-       <div class="bp-mini__thumbnail">
+       <div class="bp-mini__thumbnail"> -->
 
         <!--====== Image Code ======-->
 
-        <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html">
+        <!-- <a class="aspect aspect--bg-grey aspect--1366-768 u-d-block" href="blog-detail.html">
 
-         <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_5.jpg')}}" alt=""></a>
+         <img class="aspect__img" src="{{ asset('user_app/assets/images/blog/post_5.jpg')}}" alt=""></a> -->
         <!--====== End - Image Code ======-->
-       </div>
+       <!-- </div> -->
        <!-- <div class="bp-mini__content">
                                         <div class="bp-mini__stat">
 
@@ -200,7 +200,7 @@
                                             <a>Health</a>
                                         </div> -->
 
-       <span class="bp-mini__h1">
+       <!-- <span class="bp-mini__h1">
 
         <a href="blog-detail.html">Tell me difference between smoke and vape</a></span>
        <p class="bp-mini__p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
@@ -208,13 +208,13 @@
 
         <a class="gl-tag btn--e-transparent-hover-brand-b-2">VIEW</a>
 
-        <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a>
+        <a class="gl-tag btn--e-transparent-hover-brand-b-2">Add to cart</a> -->
 
         <!-- <a class="gl-tag btn--e-transparent-hover-brand-b-2">Place</a> -->
-       </div>
+       <!-- </div>
       </div>
      </div>
     </div>
    </div>
   </div>
- </div>
+ </div> -->
--- resources/views/user_layouts/feature.blade.php
+++ resources/views/user_layouts/feature.blade.php
@@ -205,3 +205,33 @@
   </div>
   <!--====== End - Section Content ======-->
  </div>
+
+
+
+
+
+
+
+
+ <!-- new banner section -->
+ <div class="new_banner_section row">
+  <div class="col-md-1">
+
+  </div>
+
+<div class="col-md-5">
+    <img src="{{ asset('user_app/assets/images/banners/new_banner__6.png')}}" alt="banner">
+</div>
+
+<div class="col-md-5 banner_texts">
+    <h2>Our original perfume</h2><br/>
+    <p>The new fragrance</p><br/>
+    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span><br/>
+
+    <a href="#" type="button">SHOP NOW</a>
+</div>
+
+</div>
+
+
+<!-- end banner section -->
