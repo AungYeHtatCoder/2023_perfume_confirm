8db63cd00b46d57a2c7d854e085e1319ff3ee9d5
ThaeSuAung20 <thaesuaung552000@gmail.com>
2023-09-06 16:36:50 +0630

    shop now filter

--- resources/views/frontend/shop.blade.php
+++ resources/views/frontend/shop.blade.php
@@ -2,6 +2,41 @@
 
   @section('title', 'Shop')
 
+  @section('css')
+  <style>
+/* Customize the pagination links */
+.pagination {
+ display: flex;
+ list-style: none;
+ padding: 0;
+}
+
+.pagination li {
+ margin-right: 10px;
+}
+
+.pagination a {
+ text-decoration: none;
+ background-color: #FF4500;
+ color: #fff;
+ padding: 5px 10px;
+ border-radius: 5px;
+}
+
+.pagination li .active {
+ background-color: #000;
+}
+
+.pagination a:hover {
+ background-color: #fa6c14;
+}
+
+/* Customize the active page */
+/* .pagination .page-item .active a {
+ background-color: #000 !important;
+} */
+  </style>
+  @endsection
   @section('content')
 
   <div class="app-content">
@@ -11,135 +46,23 @@
     <div class="container">
      <div class="row">
       <div class="col-lg-3 col-md-12">
-       <div class="shop-w-master">
-        <h1 class="shop-w-master__heading u-s-m-b-30"><i class="fas fa-filter u-s-m-r-8"></i>
-
-         <span>FILTERS</span>
-        </h1>
-        <div class="shop-w-master__sidebar">
-         <div class="u-s-m-b-30">
-          <div class="shop-w shop-w--style">
-           <div class="shop-w__intro-wrap">
-            <h1 class="shop-w__h">SCENTS</h1>
-
-            <span class="fas fa-minus shop-w__toggle" data-target="#s-category" data-toggle="collapse"></span>
-           </div>
-           <div class="shop-w__wrap collapse show" id="s-category">
-            <ul class="shop-w__category-list gl-scroll">
-
-             <li class="has-list">
-              <a href="#">All</a>
-              <!-- <span class="category-list__text u-s-m-l-6">(23)</span> -->
-
-              <!-- <span class="js-shop-category-span is-expanded fas fa-plus u-s-m-l-6"></span> -->
-             </li>
-             @foreach($scents as $scent)
-             <li class="has-list">
-
-              <a href="#">{{ $scent->scent_name}}</a>
-
-              <!-- <span class="category-list__text u-s-m-l-6">(5)</span> -->
-
-              <!-- <span class="js-shop-category-span fas fa-plus u-s-m-l-6"></span> -->
-
-             </li>
-             @endforeach
-             <!-- <li class="has-list">
-
-              <a href="#">Women</a>
-
-              <span class="category-list__text u-s-m-l-6">(5)</span>
-
-              <span class="js-shop-category-span fas fa-plus u-s-m-l-6"></span>
-
-             </li> -->
-
-            </ul>
-           </div>
-          </div>
-         </div>
-
-         <div class="u-s-m-b-30">
-          <div class="shop-w shop-w--style">
-           <div class="shop-w__intro-wrap">
-            <h1 class="shop-w__h">SIZE</h1>
-
-            <span class="fas fa-minus collapsed shop-w__toggle" data-target="#s-size" data-toggle="collapse"></span>
-           </div>
-           <div class="shop-w__wrap collapse" id="s-size">
-            <ul class="shop-w__list gl-scroll">
-             @foreach($sizes as $size)
-             <li>
-
-              <!--====== Check Box ======-->
-
-              <div class="check-box">
-
-               <input type="checkbox" id="xs">
-               <div class="check-box__state check-box__state--primary">
-
-                <label class="check-box__label" for="xs">{{ $size->name }}</label>
-               </div>
-              </div>
-              <!--====== End - Check Box ======-->
-
-              <!-- <span class="shop-w__total-text">(2)</span> -->
-             </li>
-             @endforeach
-            </ul>
-           </div>
-          </div>
-         </div>
-
-
-         <div class="u-s-m-b-30">
-          <div class="shop-w shop-w--style">
-           <div class="shop-w__intro-wrap">
-            <h1 class="shop-w__h">Brand</h1>
-
-            <span class="fas fa-minus collapsed shop-w__toggle" data-target="#s-price" data-toggle="collapse"></span>
-           </div>
-           <div class="shop-w__wrap collapse" id="s-price">
-            <ul class="shop-w__list gl-scroll">
-             @foreach($brands as $brand)
-             <li>
-
-              <!--====== Check Box ======-->
-              <div class="check-box">
-
-               <input type="checkbox" id="dior">
-               <div class="check-box__state check-box__state--primary">
-
-                <label class="check-box__label" for="dior">{{ $brand->brand_name }}</label>
-               </div>
-              </div>
-              <!--====== End - Check Box ======-->
-
-              <!-- <span class="shop-w__total-text">(2)</span> -->
-             </li>
-             @endforeach
-            </ul>
-           </div>
-          </div>
-         </div>
-        </div>
-       </div>
+       @include('frontend.shop_sidebar');
       </div>
       <div class="col-lg-9 col-md-12">
        <div class="shop-p">
         <div class="shop-p__toolbar u-s-m-b-30">
-         <!-- <div class="shop-p__meta-wrap u-s-m-b-60">
+         <div class="shop-p__meta-wrap u-s-m-b-30">
           <div class="shop-p__meta-text-2">
 
-           <span>Related Searches:</span>
+           <span>{{ $filter}}</span>
 
-           <a class="gl-tag btn--e-brand-shadow" href="#">Popular</a>
+           <!-- <a class="gl-tag btn--e-brand-shadow" href="#">Popular</a>
 
            <a class="gl-tag btn--e-brand-shadow" href="#">Men Ranges</a>
 
-           <a class="gl-tag btn--e-brand-shadow" href="#">Women Ranges</a>
+           <a class="gl-tag btn--e-brand-shadow" href="#">Women Ranges</a> -->
           </div>
-         </div> -->
+         </div>
          <!-- <div class="shop-p__tool-style">
           <form>
            <div class="tool-style__form-wrap">
@@ -162,7 +85,8 @@
            <div class="product-m">
             <div class="product-m__thumb">
 
-             <a class="aspect aspect--bg-grey aspect--square u-d-block" href="{{url('/product_detail') }}">
+             <a class="aspect aspect--bg-grey aspect--square u-d-block"
+              href="{{url('/product_detail/'.$product->id) }}">
 
               <img class="aspect__img" src="{{ asset('assets/img/products/'.$product->image)}}" alt=""></a>
              <div class="product-m__quick-look">
@@ -217,29 +141,11 @@
         <div class="u-s-p-y-60">
 
          <!--====== Pagination ======-->
-         <ul class="shop-p__pagination">
-          <li class="is-active">
-
-           <a href="shop-side-version-2.html">1</a>
-          </li>
-          <li>
-
-           <a href="shop-side-version-2.html">2</a>
-          </li>
-          <li>
-
-           <a href="shop-side-version-2.html">3</a>
-          </li>
-          <li>
-
-           <a href="shop-side-version-2.html">4</a>
-          </li>
-          <li>
-
-           <a class="fas fa-angle-right" href="shop-side-version-2.html"></a>
-          </li>
-         </ul>
+         <div style="display: flex; justify-content: center;">
+          {{ $products->links() }}
+         </div>
          <!--====== End - Pagination ======-->
+
         </div>
        </div>
       </div>
