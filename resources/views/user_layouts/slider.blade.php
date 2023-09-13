56f201d1ffc3dabe810c5f20fda37993858320ae
eithandarmon <eithandarmon9911@gmail.com>
2023-09-13 10:59:36 +0630

    carousel update

--- resources/views/user_layouts/slider.blade.php
+++ resources/views/user_layouts/slider.blade.php
@@ -1,4 +1,4 @@
- <div class="s-skeleton s-skeleton--h-600 s-skeleton--bg-grey">
+ <!-- <div class="s-skeleton s-skeleton--h-600 s-skeleton--bg-grey">
   <div class="owl-carousel primary-style-1" id="hero-slider">
    <div class="hero-slide hero-slide--1">
     <div class="container">
@@ -17,7 +17,8 @@
       </div>
      </div>
     </div>
-   </div>
+   </div> -->
+
    <!-- <div class="hero-slide hero-slide--2">
     <div class="container">
      <div class="row">
@@ -64,5 +65,50 @@
      </div>
     </div>
    </div> -->
-  </div>
- </div>
+
+  <!-- </div>
+ </div> -->
+
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="{{ asset('user_app/assets/images/banners/sample_1.png')}}" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+
+                <h5>Latest Update Stock</h5>
+                <h2>Promotion on Perfume</h2>
+                <h6>Find perfume on best prices, Also Discover most selling products of perfume</h6>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('user_app/assets/images/banners/sample_2.png')}}"  alt="Second slide">
+            <div class="carousel-caption d-none d-md-block"  style="color:black;">
+                <h5>Latest Update Stock</h5>
+                <h3>Promotion on Perfume</h3>
+                <h6>Find perfume on best prices, Also Discover most selling products of perfume</h6>
+                <a type="button" href="#" class="btn btn-info mt-3">Shop Now</a>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('user_app/assets/images/banners/sample_3.png')}} " alt="Third slide" >
+            <div class="carousel-caption d-none d-md-block">
+                <h4>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h4>
+                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham</p>
+                
+              </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
